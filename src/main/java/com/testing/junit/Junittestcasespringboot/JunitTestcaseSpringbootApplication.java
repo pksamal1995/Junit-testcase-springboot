b1d4cb96896529bf6b2b2d1923315288a42833df
Pritam Kumar Samal <pritam.samal@s-force.org>
2020-02-25 23:16:04 +0530

    second commit

---
+++ src/main/java/com/testing/junit/Junittestcasespringboot/JunitTestcaseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.testing.junit.Junittestcasespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestcaseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestcaseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/junit/Junittestcasespringboot/JunitTestcaseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.junit.Junittestcasespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestcaseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
