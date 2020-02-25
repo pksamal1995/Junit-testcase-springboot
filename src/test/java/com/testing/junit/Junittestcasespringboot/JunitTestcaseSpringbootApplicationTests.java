28adcc037b11ae10cabfce27549a206e42075967
Pritam Kumar Samal <pritam.samal@s-force.org>
2020-02-26 01:50:29 +0530

    test case passed

--- src/test/java/com/testing/junit/Junittestcasespringboot/JunitTestcaseSpringbootApplicationTests.java
+++ src/test/java/com/testing/junit/Junittestcasespringboot/JunitTestcaseSpringbootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.testing.junit.Junittestcasespringboot;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JunitTestcaseSpringbootApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		 mvc.perform(MockMvcRequestBuilders.get("/getall"))
+         .andExpect(status().isOk())
+         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 
 }
